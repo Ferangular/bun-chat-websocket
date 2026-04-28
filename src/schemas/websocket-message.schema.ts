21e55717ab65676cd81f7486e217de32f87e7cf5
firthuns <firthunsnpm@gmail.com>
2026-04-28 10:30:21 +0200

    feat: esquema de validacion con zod

--- src/schemas/websocket-message.schema.ts
+++ src/schemas/websocket-message.schema.ts
@@ -1,21 +1,33 @@
 import { z } from 'zod';
 
-const typeSchema = z.enum([
-  'GET_ITEMS',
-  'ADD_ITEM',
-  'UPDATE_ITEM',
-  'DELETE_ITEM',
+export const messageSchema = z.discriminatedUnion('type', [
+  z.object({
+    type: z.literal('SEND_GROUP_MESSAGE'),
+    payload: z.object({
+      content: z.string({ required_error: 'Content is required' }),
+      groupId: z.string().optional(), // Usaré el default chat id
+    }),
+  }),
+
+  z.object({
+    type: z.literal('SEND_DIRECT_MESSAGE'),
+    payload: z.object({
+      content: z.string({ required_error: 'Content is required' }),
+      receiverId: z.string({ required_error: 'ReceiverID is required' }),
+    }),
+  }),
+
+  z.object({
+    type: z.literal('GET_GROUP_MESSAGES'),
+    payload: z.object({
+      groupId: z.string({ required_error: 'GroupID is required' }),
+    }),
+  }),
+
+  z.object({
+    type: z.literal('GET_DIRECT_MESSAGES'),
+    payload: z.object({
+      receiverId: z.string({ required_error: 'ReceiverId is required' }),
+    }),
+  }),
 ]);
-
-const payloadSchema = z.object({
-  id: z.string().optional(),
-  name: z.string().optional(),
-});
-
-export const messageSchema = z.object({
-  type: typeSchema,
-  payload: payloadSchema.optional(),
-});
-
-export type MessageParsed = z.infer<typeof messageSchema>;
-export type MessagePayload = z.infer<typeof payloadSchema>;
